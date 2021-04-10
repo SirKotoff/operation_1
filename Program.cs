fcf2c8f4bf87d446f2238f2911fbf407e07bee4e
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-04-10 21:57:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Vector_class
+{
+
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3D a = new Vector3D(3, 2, 1);
+            Vector3D b = new Vector3D(0, 1, 2);
+
+            Console.WriteLine("Дано: вектор а{0} и вектор b{1}", a.ToString(), b.ToString());
+
+            // Длина векторов
+            // Свойства
+            Console.WriteLine("Длина векторов a: [{0}]", a.Length);
+            Console.WriteLine("Длина векторов b: [{0}]", b.Length);         
+
+            // Угол между векторам (cos)
+            Console.WriteLine("Угол между вектором a и b, cos(a^b): {0}", a.Angle(b));
+
+            // Сложение векторов
+            Console.WriteLine("a{0} + b{1} = c{2}", a.ToString(), b.ToString(), a.Plus(b));
+            Console.WriteLine("a{0} + b{1} = c{2}", a.ToString(), b.ToString(), a + b);
+
+            // Умножение вектора на число
+            Console.WriteLine("{0} * a{1} = c{2}", 2, a.ToString(), a.SubDigit(2));
+            Console.WriteLine("{0} * a{1} = c{2}", 2, a.ToString(), 2 * a);
+
+            // Скалярное умножение
+            Console.WriteLine("a{0} * b{1} = {2}", a.ToString(), b.ToString(), a.SubScalar(b));
+            Console.WriteLine("a{0} * b{1} = {2}", a.ToString(), b.ToString(), a * b);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vector3D.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_class
+{
+    public class Vector3D
+    {
+
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public Vector3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+
+
+
+
+        public double Length
+        {
+            get
+            {
+                return Math.Round(Math.Sqrt(X * X + Y * Y + Z * Z), 2);
+            }
+            set
+            {
+                this.Length = value;
+            }
+        }
+
+
+
+
+        //угол между двумя векторами
+        public double Angle(Vector3D v)
+        {
+            double cos = Math.Round((X * v.X + Y * v.Y + Z * v.Z) / (this.Length * v.Length), 4);
+            return cos;
+        }
+
+
+        // Сложение векторов
+        public Vector3D Plus(Vector3D v)
+        {
+            double xx = X + v.X;
+            double yy = Y + v.Y;
+            double zz = Z + v.Z;
+            return new Vector3D(xx, yy, zz);
+        }
+        // Переопределение операции +
+        public static Vector3D operator +(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+
+        // Вычитание векторов
+        public Vector3D Minus(Vector3D v)
+        {
+            double xx = X - v.X;
+            double yy = Y - v.Y;
+            double zz = Z - v.Z;
+            return new Vector3D(xx, yy, zz);
+        }
+        // Переопределение операции -
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+
+
+        // Умножение вектора на число
+        public Vector3D SubDigit(double digit)
+        {
+            return new Vector3D(X * digit, Y * digit, Z * digit);
+        }
+        // Переопределение операции *
+        public static Vector3D operator *(double digit, Vector3D v1)
+        {
+            return new Vector3D(digit * v1.X, digit * v1.Y, digit * v1.Z);
+        }
+
+
+        // Скалярное умножение векторов
+        public double SubScalar(Vector3D v)
+        {
+            return Math.Round(this.Length * v.Length * this.Angle(v), 2);
+        }
+        // Переопределение операции *
+        public static double operator *(Vector3D v1, Vector3D v2)
+        {
+            return Math.Round(v1.Length * v2.Length * v1.Angle(v2), 2);
+        }
+
+
+        // Векторное умножение векторов
+
+        public Vector3D SubVector(Vector3D v)
+        {
+            return new Vector3D((Y * v.Z - Z * v.Y), (-1) * (X * v.Z - Z * v.X), (X * v.Y - Y * v.X));
+        }
+
+
+        // Переопределение метода ToString()
+        public override string ToString()
+        {
+            return String.Format("({0}, {1}, {2})", X, Y, Z);
+        }
+
+
+
+    }
+}
